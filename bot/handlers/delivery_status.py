e1c3d4e3fedd89f093155467de38fcda8b8091e5
19taurus79 <19taurus79@gmail.com>
2023-08-21 12:41:41 +0300

    Добавил проверку статуса

---
+++ bot/handlers/delivery_status.py
@@ -0,0 +1,29 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message, ReplyKeyboardRemove
+from utils.message.submissions_answer import submissions_answer
+from aiogram.fsm.state import StatesGroup, State
+from handlers.remains import CommandBot
+from aiogram.fsm.context import FSMContext
+from handlers.remains import CommandBot
+from middlewares.user_validator import managers_id
+
+from bot.utils.message.submissions_answer import submissions_delivery_status
+
+router = Router()
+
+
+@router.message(Command("delivery_status"))
+async def delivery_status(message: Message):
+    await message.delete()
+    manager_id = message.from_user.id
+    manager = next(ch for ch, code in managers_id.items() if code == manager_id)
+    await submissions_delivery_status(message, manager)
+
+
+
+@router.message(CommandBot.choosing_submissions_nomenclature)
+async def submission_with_nomenclature(message: Message, state: FSMContext):
+    nomenclature = message.text
+    await submissions_answer(message, nomenclature)
+    await state.clear()
--- bot/utils/message/submissions_answer.py
+++ bot/utils/message/submissions_answer.py
@@ -4,6 +4,8 @@ from aiogram.types import ReplyKeyboardRemove
 from utils.db.submissions import get_submissions, quantity_under_orders
 from aiogram.exceptions import TelegramBadRequest as err
 
+from bot.utils.db.submissions import get_submissions_delivery_status
+
 
 async def submissions_answer(message, val):
     await message.answer(f"<strong>*****Заявки*****</strong>{chr(10)}{chr(10)}")
@@ -45,3 +47,30 @@ async def submissions_answer(message, val):
             )
     if len(sub) == 0:
         await message.answer("Заявок нет", reply_markup=ReplyKeyboardRemove)
+
+async def submissions_delivery_status(message, manager):
+
+    b = []
+    sub = await get_submissions_delivery_status(manager)
+
+    if len(sub) > 0:
+        for i in range(len(sub)):
+                b.append(
+                    f"<i>{sub[i].get('client')}</i>{chr(10)}{sub[i].get('product.product')} {sub[i].get('different')}{chr(10)}{chr(10)}"
+                )
+        try:
+            await message.answer(f"<strong>***** Заявки с семенами и статусом 'Ні до постачання' *****</strong>{chr(10)} У менеджера {manager}{chr(10)}")
+            await message.answer("".join(b), reply_markup=ReplyKeyboardRemove)
+            text = "".join(b)
+            logging.info(f"Пользователь {message.from_user.id} получил ответ {text}")
+        except err:
+            await message.answer(
+                f"Вероятно слишком длинное сообщение{chr(10)}"
+                f"Попробуйте конкретизировать данные для поиска"
+            )
+            logging.info(
+                f"Вероятно слишком длинное сообщение{chr(10)}"
+                f"Попробуйте конкретизировать данные для поиска"
+            )
+    if len(sub) == 0:
+        await message.answer("Заявок с таким статусом нет", reply_markup=ReplyKeyboardRemove)
